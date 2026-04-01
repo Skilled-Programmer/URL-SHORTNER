838965a1d3c5cb6dd8770f7ffdbfbd1179284564
Abhishek Mishra <abhishekraja122005@gmail.com>
2026-04-01 12:39:01 +0530

    changed the render url

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import {connectDb} from "./db.js";
 import {getDb} from "./db.js";
 dotenv.config();
 const app=express();
+const baseUrl="https://url-shortner-ez48.onrender.com";
 app.use(express.json());
 const PORT=process.env.PORT;
 app.use(express.urlencoded({extended:true}));
@@ -13,8 +14,8 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
 const errorPage404=path.join(import.meta.dirname,"public","errorPage404.html")
 await connectDb();
-app.post("/shorten",saveLink);
-app.get("/:shortCode",async (req,res)=>{
+app.post(`${baseUrl}/shorten`,saveLink);
+app.get(`${baseUrl}/:shortCode`,async (req,res)=>{
     let db=getDb();
     const shortCode = req.params.shortCode;
     let getUrl=await db.collection("urls").findOne({shortCode:shortCode});

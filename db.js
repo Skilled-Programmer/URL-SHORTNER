43086bbdc538ebf5bf97bcc43a45912aaec8b826
Abhishek Mishra <abhishekraja122005@gmail.com>
2026-04-28 13:06:25 +0530

    db chnaged

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb=async ()=>{
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/urlShortner");
+        await mongoose.connect(process.env.MONGO_URI);
     } catch (error) {
         console.error(error);
     }
